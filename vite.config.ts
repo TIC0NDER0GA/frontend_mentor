3efb5409d04938f06dc70fde66fe9f5877e824e4
TIC0NDER0GA <Tougherword@gmail.com>
2025-05-17 17:43:45 -0400

    blogpost gh commit

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  //base: '/frontend_mentor/',
-  base: '/',
+  base: '/frontend_mentor/',
+  // base: '/',
   plugins: [react()],
    build: {
     outDir: 'docs',       
